abb4b988e4b7eccebc695d36586e20a1e8dbb382
WGuandique-2005 <wguandiquerivera07@gmail.com>
2024-10-10 17:17:22 -0600

    first commit

--- main_project.py
+++ main_project.py
@@ -20,7 +20,7 @@ try:
         database=database
     )
 except mysql.connector.Error as err:
-    app.logger.error("Error connecting to MySQL: %s", err)
+    app.logger.error("Error de conexión a MySQL: %s", err)
     exit(1)
 
 
@@ -43,13 +43,13 @@ def login():
                 session['logged_in'] = True
                 return redirect(url_for('home'))
             else:
-                app.logger.error("Incorret password, try again!")
-                return "Invalid password"
+                app.logger.error("Contraseña incorrecta, intente de nuevo!")
+                return "Contraseña invalida"
         else:
-            return "Invalid username"
+            return "Nombre de usuario invalido"
     except mysql.connector.Error as err:
-        app.logger.error("Error executing query: %s", err)
-        return "Error executing query"
+        app.logger.error("Error en la ejecución: %s", err)
+        return "Error en la ejecución"
     finally:
         cursor.close()
 
@@ -80,18 +80,18 @@ def gestionar():
     status = request.form.get("estado")
         
     if not all([title, author, genr, editorial, synopsis, date, status]):
-        app.logger.error("Error: Missing required fields")
-        return "Error: Please fill in all required fields"
+        app.logger.error("Error: Campos necesarios vacíos")
+        return "Error: Porfavor llene todos los campos"
     try:
         cursor = mysql_conn.cursor()
         query = "INSERT INTO libro (titulo, autor, genero, editorial, sinopsis, fecha_publicacion, estado) VALUES (%s, %s, %s, %s, %s, %s, %s)"
         cursor.execute(query, (title, author, genr, editorial, synopsis, date, status))
         mysql_conn.commit()
-        app.logger.info("Book added successfully")
+        app.logger.info("Libro agregado con éxito")
         return "Libro agregado con éxito"
     except mysql.connector.Error as error:
-        app.logger.error("Error executing query: %s", error)
-        return "Error executing query"
+        app.logger.error("Error en la ejecución: %s", error)
+        return "Error en la ejecución"
     finally:
         cursor.close()
 
@@ -109,22 +109,22 @@ def reg():
     pw2 = request.form.get("re_pw")
     
     if not all([name, lastname, un, email, pw, pw2]):
-        app.logger.error("Error: Missing required fields")
-        return "Error: Please fill in all required fields"
+        app.logger.error("Error: Campos necesarios vacíos")
+        return "Error: Porfavor llene todos los campos"
     try:
         if pw != pw2:
-            app.logger.error("Error: Passwords do not match")
-            return "Error: Passwords do not match"
+            app.logger.error("Error: La contraseña no coincide")
+            return "Error: La contraseña no coincide"
         else:
             hashed_pw = bcrypt.hashpw(pw.encode('utf-8'), bcrypt.gensalt(12))
             cursor = mysql_conn.cursor()
             query = "INSERT INTO usuario (nombre, apellido, user_name, email, contraseña) VALUES (%s, %s, %s, %s, %s)"
             cursor.execute(query, (name, lastname, un, email, hashed_pw))
             mysql_conn.commit()
-            app.logger.info("User  registered successfully")
+            app.logger.info("Usuario registrado correctamenete!")
             return redirect(url_for('index'))    
     except mysql.connector.Error as err:
-        app.logger.error("Error executing query: %s", err)
+        app.logger.error("Error en la ejecución: %s", err)
     finally:
         cursor.close()
 
